237bb5af8f48eb34a514e3c89472772e8b0dc836
Pavlo <pavdemchnko@gmail.com>
2025-02-04 13:50:54 -0500

    Added copy function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Enter the source file path: ");
+        string sourcePath = Console.ReadLine();
+
+        if (!File.Exists(sourcePath))
+        {
+        Console.WriteLine("File not found! Make sure the path is correct.");
+        return;
+        }
+
+        Console.Write("Enter the path to the destination file: ");
+        string destinationPath = Console.ReadLine();
+
+        File.Copy(sourcePath, destinationPath, true);
+        Console.WriteLine("File copied successfully!");
+    }
+}
